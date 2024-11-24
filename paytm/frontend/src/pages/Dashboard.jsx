b0a7fec3b9fc9c76df06f255a806a5b9b5e3e424
amriith <amrithjayadeep916@gmail.com>
2024-11-24 20:00:38 +1100

    front End changes connected backend and dashboard page

--- paytm/frontend/src/pages/Dashboard.jsx
+++ paytm/frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,29 @@
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import axios from "axios"; 
+import { useEffect, useState } from "react";
+
 
 export const Dashboard = () => {
+ const [balance, setBalance]=useState("");
+ const token = localStorage.getItem("token");
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+    })
+            .then(response => {
+                console.log("Full API Response:", response.data);
+                setBalance(response.data.balance || []);
+            })
+    }, [])
+
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10,000"} />
+            <Balance value={balance} />
             <Users />
         </div>
     </div>
